482c2da259a14544cab18bdbc09508e390e1dc0e
bogusz <bogusz2005@gmail.com>
2024-05-23 18:16:48 +0200

    czemu działa getcard i czemu nie działa get my cards WTF

--- src/main/kotlin/me/szydelko/DAO/dao.kt
+++ src/main/kotlin/me/szydelko/DAO/dao.kt
@@ -30,7 +30,9 @@ class Room(owner: ConnectionWS, val cardSet: List<CardItem> = CardSet.default) {
     val cardList = mutableMapOf(lastCard to 1);
 
     fun ifCardIsInDeck(card: Card) : Boolean{
-        return ((cardList.filterKeys { it == card }.values.firstOrNull() ?: throw Exception("nie ma takiej karty w wykorzystywanych kartach (card list)")) < (cardSet.find { it.toCard() == card }!!.count)) // @TODO xd exception
+        val i = cardSet.find { it.toCard() == card }?.count ?: throw Exception("nie ma takiej karty w tali")
+        val i1 = cardList.filterKeys { it == card }.values.firstOrNull() ?: return (i > 0)
+        return (i1 < i) // @TODO xd exception
     }
 
 //    fun getNextCard(): Card{
